92652efbc34bc2b06437e24414f2b3778aa6e419
mindatama <mindatama@gmail.com>
2020-12-17 13:13:12 +0800

    main.dart menambah file main.dart pada branch master

---
+++ main.dart
@@ -0,0 +1,8 @@
+main() {
+  print("hello world");
+  final jargon = "bisa, harus bisa, pasti bisa";
+  var hehe = "semangat pagi";
+  var haha = 'hallo dunia,\' saya wijatmoko. $hehe';
+  print(haha);
+  print(jargon);
+}
