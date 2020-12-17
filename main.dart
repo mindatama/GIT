de07bc43f24480e8ae283f817076cfa3f08df51d
mindatama <mindatama@gmail.com>
2020-12-17 13:27:06 +0800

    main.dart menembah variabel const

--- main.dart
+++ main.dart
@@ -1,8 +1,12 @@
 main() {
-  print("hello world");
+  print("indonesia");
   final jargon = "bisa, harus bisa, pasti bisa";
   var hehe = "semangat pagi";
-  var haha = 'hallo dunia,\' saya wijatmoko. $hehe';
+  var haha = 'hallo dunia,\' saya wijatmoko. $hehe $jargon';
   print(haha);
   print(jargon);
+  const bilangan = 3;
+  const double hasil = 1000 / bilangan;
+  print(bilangan);
+  print(hasil);
 }
