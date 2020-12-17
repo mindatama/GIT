c05b69a9f35598d3f5eeedd1f96fb95a154a73b4
mindatama <mindatama@gmail.com>
2020-12-17 13:10:18 +0800

    main.dart menambah variabel final

--- main.dart
+++ main.dart
@@ -1,3 +1,8 @@
 main() {
   print("helloworld");
+  final jargon = "bisa, harus bisa, pasti bisa";
+  var hehe = "semangat pagi";
+  var haha = 'hallo dunia,\' saya wijatmoko. $hehe';
+  print(haha);
+  print(jargon);
 }
