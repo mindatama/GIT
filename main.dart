215e37b63096b5dda2cc6b64a9b74bb35d754a00
mindatama <mindatama@gmail.com>
2020-12-19 00:49:52 +0800

    main.dart merapikan kode sebelum tidur

--- main.dart
+++ main.dart
@@ -1,18 +1,22 @@
 void main() {
   //print hello world versi indonesia
   print("indonesia");
+
   //variabel final
   final jargon = "bisa, harus bisa, pasti bisa";
-  //variabel var
+
+//variabel var
   var hehe = "semangat pagi";
   var haha = 'hallo dunia,\' saya wijatmoko. $hehe $jargon';
   print(haha);
   print(jargon);
+
   //variabel const
   const bilangan = 3;
   const double hasil = 1000 / bilangan;
   print(bilangan);
   print(hasil);
+
   // IF
   print("IF berapa hasil dari 4+4?");
   int hasilif = 8;
@@ -20,6 +24,7 @@ void main() {
   if (hasilif == 8) {
     print("benar!");
   }
+
   // IF ELSE
   print("IF ELSE bilangan: 10 benar, selain 10 salah");
   int hasilifelse = 8;
@@ -29,10 +34,10 @@ void main() {
   } else {
     print("salah");
   }
+
   // IF ELSE IF ELSE
   int point = 29;
   print("point: $point");
-
   var grade;
   if (point >= 80) {
     grade = "A";
@@ -53,6 +58,7 @@ void main() {
   print("hasil: $hasilnya");
   var hasilce = hasilnya == 2 ? "benar!" : "salah";
   print(hasilce);
+
   // conditional expresion 2
   int angka1 = 2;
   int angka2 = 3;
