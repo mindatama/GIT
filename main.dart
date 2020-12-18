2b07ea6659f0c97bb3ab3328b5d171b65dc33fb2
mindatama <mindatama@gmail.com>
2020-12-18 10:56:35 +0800

    main.dart tambah if, ifelse, ifelseif

--- main.dart
+++ main.dart
@@ -1,12 +1,49 @@
 main() {
+  //print hello world versi indonesia
   print("indonesia");
+  //variabel final
   final jargon = "bisa, harus bisa, pasti bisa";
+  //variabel var
   var hehe = "semangat pagi";
   var haha = 'hallo dunia,\' saya wijatmoko. $hehe $jargon';
   print(haha);
   print(jargon);
+  //variabel const
   const bilangan = 3;
   const double hasil = 1000 / bilangan;
   print(bilangan);
   print(hasil);
+  // IF
+  print("IF berapa hasil dari 4+4?");
+  int hasilif = 8;
+  print(hasilif);
+  if (hasilif == 8) {
+    print("benar!");
+  }
+  // IF ELSE
+  print("IF ELSE bilangan: 10 benar, selain 10 salah");
+  int hasilifelse = 8;
+  print("input hasil ifelse $hasilifelse");
+  if (hasilifelse == 10) {
+    print("benar");
+  } else {
+    print("salah");
+  }
+  // IF ELSE IF ELSE
+  int point = 29;
+  print("point: $point");
+
+  var grade;
+  if (point >= 80) {
+    grade = "A";
+  } else if (point >= 60) {
+    grade = "B";
+  } else if (point >= 40) {
+    grade = "C";
+  } else if (point >= 20) {
+    grade = "D";
+  } else {
+    grade = "E";
+  }
+  print("grade: $grade");
 }
