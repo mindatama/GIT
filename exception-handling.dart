0735458594e140cb7c0da61b7d109dd2a3a0232c
mindatama <mindatama@gmail.com>
2020-12-22 22:58:17 +0800

    excetion-handling memodifikasi contoh try catch finally

--- exception-handling.dart
+++ exception-handling.dart
@@ -1,4 +1,5 @@
 void main() {
+  print("contoh try on");
   try {
     int hasil = 12 ~/ 0;
     print("hasilnya: $hasil");
@@ -6,13 +7,17 @@ void main() {
     print("tidak bisa dibagi 0");
   }
 
+  print("\ncontoh try catch finally");
   try {
-    int hasil = 12 ~/ 0;
-    print("hasilnya: $hasil");
+    luas(3, 0);
   } catch (aneh, s) {
     print("errornya: $aneh");
     print("stack trace \n $s");
   } finally {
     print("bagian selalu dieksekusi");
   }
+}
+
+void luas(int panjang, int lebar) {
+  print("luas: ${panjang ~/ lebar}");
 }
