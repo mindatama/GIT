2dc0405d602feb68b769a8c02c733d01ea83eb45
mindatama <mindatama@gmail.com>
2020-12-24 20:47:09 +0700

    mainclass.dart tambah file baru contoh class pada dart

---
+++ mainclass.dart
@@ -0,0 +1,26 @@
+void main() {
+  var mahasiswa1 = murid();
+  print("${mahasiswa1.nama} ${mahasiswa1.nik}");
+  mahasiswa1.belajar();
+  mahasiswa1.kerja();
+
+  var mahasiswa2 = murid();
+  mahasiswa2.nik = 10023555;
+  mahasiswa2.nama = 'mindatama';
+  print("${mahasiswa2.nama} ${mahasiswa2.nik}");
+  mahasiswa2.kerja();
+  mahasiswa2.belajar();
+}
+
+class murid {
+  int nik = 10023543;
+  String nama = 'wijatmoko';
+
+  void belajar() {
+    print('${this.nama} sedang belajar');
+  }
+
+  void kerja() {
+    print('${this.nama} sedang kerja');
+  }
+}
