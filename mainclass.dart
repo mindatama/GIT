fa43fbe44d2673da1f49e5b0ddb0bb7d8b8006d1
mindatama <mindatama@gmail.com>
2020-12-26 11:24:05 +0700

    mainclass.dart penambaan contoh parameter constructor, untuk constructor hanya bisa 1 tiap class dan tidak bisa double constructor

--- mainclass.dart
+++ mainclass.dart
@@ -1,12 +1,12 @@
 void main() {
-  var mahasiswa1 = murid();
+  var mahasiswa1 = murid(10023543, 'wijatmoko');
   print("${mahasiswa1.nama} ${mahasiswa1.nik}");
   mahasiswa1.belajar();
   mahasiswa1.kerja();
 
-  var mahasiswa2 = murid();
-  mahasiswa2.nik = 10023555;
-  mahasiswa2.nama = 'dewi chariratul latifah';
+  var mahasiswa2 = murid(10023555, 'dewi');
+  // mahasiswa2.nik = 10023555;
+  // mahasiswa2.nama = 'dewi chariratul latifah';
   print("${mahasiswa2.nama} ${mahasiswa2.nik}");
   mahasiswa2.kerja();
   mahasiswa2.belajar();
@@ -16,9 +16,15 @@ class murid {
   int nik = 10023543;
   String nama = 'wijatmoko';
 
-//default constructor
-  murid() {
-    print('default constructor');
+// default constructor
+//   murid() {
+//     print('default constructor');
+//   }
+
+//   parameter constructor
+  murid(int nik, String nama) {
+    this.nik = nik;
+    this.nama = nama;
   }
   void belajar() {
     print('${this.nama} sedang belajar');
