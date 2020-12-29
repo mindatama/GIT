a3a92f51958d67b364b9ea3eb9afbe01b615af2d
mindatama <mindatama@gmail.com>
2020-12-29 21:22:57 +0700

    named construct pada inheritance

--- test.dart
+++ test.dart
@@ -1,5 +1,5 @@
 void main() {
-  var anggora = kucing();
+  var anggora = kucing.namedconstruct("hijau");
   anggora.hidup();
   // anggora.umur = 12;
   // anggora.jenis = 'anggora jinak';
@@ -7,14 +7,22 @@ void main() {
 }
 
 class hewan {
+  String warna;
   int umur;
   void hidup() {
     print('hewan darat bernafas dengan paru paru');
+  }
+
+  hewan.namedconstruct(String warna) {
+    this.warna = warna;
+    print("");
   }
 }
 
 class kucing extends hewan {
   String jenis;
+
+  kucing.namedconstruct(String warna) : super.namedconstruct(warna);
   void hidup() {
     super.hidup();
     print('kucing hewan bernyawa');
