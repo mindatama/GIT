8775dec64715c247ffc2e9e6ef6c5763d316832e
mindatama <mindatama@gmail.com>
2020-12-30 15:31:46 +0700

    test.dart merubah menjadi contoh static double dan void pada class hewan

--- test.dart
+++ test.dart
@@ -1,73 +1,13 @@
 void main() {
-  var anggora = kucing.namedconstruct("hijau");
-  anggora.hidup();
   hewan.umur = 12;
-  anggora.jenis = 'anggora jinak';
-  print('${anggora.jenis} umurnya adalah ${hewan.umur} tahun');
-
-  var mawarmerah = mawar();
-  mawarmerah.bergerak();
-
-  var butuh = kebutuhan();
-  butuh.mesin();
-  butuh.water();
+  print('umurnya adalah ${hewan.umur} tahun');
+  hewan.hidup();
 }
 
 class hewan {
   String warna;
   static int umur;
-  void hidup() {
+  static void hidup() {
     print('hewan darat bernafas dengan paru paru');
-  }
-
-  hewan.namedconstruct(String warna) {
-    this.warna = warna;
-    print("");
-  }
-}
-
-class kucing extends hewan {
-  String jenis;
-
-  kucing.namedconstruct(String warna) : super.namedconstruct(warna);
-  void hidup() {
-    super.hidup();
-    print('kucing hewan bernyawa');
-  }
-}
-
-//contoh abstract class& abstract method
-
-abstract class tumbuhan {
-  void bergerak();
-}
-
-class mawar extends tumbuhan {
-  void bergerak() {
-    print("menghadap matahari");
-  }
-}
-
-//contoh interface
-
-class nature {
-  void water() {
-    print("air bersih");
-  }
-}
-
-class industry {
-  void mesin() {
-    print("AI");
-  }
-}
-
-class kebutuhan implements nature, industry {
-  void water() {
-    print("air konsumsi");
-  }
-
-  void mesin() {
-    print("tensorflow");
   }
 }
