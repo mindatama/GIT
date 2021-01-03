2982dc8b8ac0669bd84f3eda3247cbff59aef6b0
mindatama <mindatama@gmail.com>
2021-01-02 22:13:16 -0200

    tambah folder rnd_dart belajar oop dart mentor erico darmawan

---
+++ rnd_dart/bin/rnd_dart.dart
@@ -0,0 +1,5 @@
+import 'package:rnd_dart/rnd_dart.dart' as rnd_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rnd_dart.calculate()}!');
+}
---
+++ rnd_dart/test/rnd_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:rnd_dart/rnd_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
