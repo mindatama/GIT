e3af212fe587b4a6cfa254a6b151c1993e7b89cc
mindatama <mindatama@gmail.com>
2021-01-02 22:57:04 -0400

    folder bin, rnd.dart sudah dimodifikasi tapi belum muncul hasil yang sesuai

--- rnd_dart/bin/rnd_dart.dart
+++ rnd_dart/bin/rnd_dart.dart
@@ -3,6 +3,6 @@ import 'dart:io';
 void main(List<String> arguments) {
   var input = stdin.readLineSync();
   // print(hehe);
-  var number = int.tryParse(input);
-  print(number + 9);
+  int number = int.tryParse(input);
+  print("${number} semrang");
 }
