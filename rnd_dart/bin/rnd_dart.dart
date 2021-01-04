58c2c5dce52a6f4f3a9f48099708ddaafb062afc
mindatama <mindatama@gmail.com>
2021-01-03 14:48:45 -1200

    folder bin. ganti contoh perulangan metode for

--- rnd_dart/bin/rnd_dart.dart
+++ rnd_dart/bin/rnd_dart.dart
@@ -1,19 +1,7 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  int number = int.tryParse(stdin.readLineSync());
-
-  switch(number){
-    case 0:
-      print("nol");
-      break;
-    case 1:
-      print("satu");
-      print(number + 10); break;
-    case 2:
-      print("dua"); break;
-    default:
-      print("bilangan lain");
-
+  for (int x = 1; x < 4; x= x+1){
+    print("hay");
   }
 }
