ae9bfa112b4926979030b0d6cf44216fd5380b69
mindatama <mindatama@gmail.com>
2021-01-04 14:26:44 +0800

    folder bin. rnd_dart tambah contoh percabangan nilai pasti (switch) dikombinasikan dengan perulangan pada outputnya

--- rnd_dart/bin/rnd_dart.dart
+++ rnd_dart/bin/rnd_dart.dart
@@ -1,8 +1,22 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  // x= x-2 di sederhanakan x-= 2
-  for (int x = 9; x > 2; x= x-2){
-    print("halo");
+  int x = int.tryParse(stdin.readLineSync());
+
+  switch (x) {
+    case 0:
+      for (int x = 0; x < 3; x= x+1){
+        print("hey ${x}"); }
+      break;
+    case 1:
+      for (int x = 1; x > -4; x= x-2){
+        print("halo ${x}"); }
+      break;
+    case 2:
+      for (int x = 2; x > -2; x-=1){
+        print("halo ${x}"); }
+      break;
+    default:
+      print("bilangan lain");
   }
 }
