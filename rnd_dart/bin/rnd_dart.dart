a6f9b60df2c6d91b62df5e580b5773c0f56ec89e
mindatama <mindatama@gmail.com>
2021-01-03 23:18:18 -0200

    folder bin. ganti contoh percabangan switch

--- rnd_dart/bin/rnd_dart.dart
+++ rnd_dart/bin/rnd_dart.dart
@@ -3,26 +3,17 @@ import 'dart:io';
 void main(List<String> arguments) {
   int number = int.tryParse(stdin.readLineSync());
 
-  //contoh if else
-  // if(number > 0) {
-  //   print('positip');
-  // } else if(number < 0) {
-  //   print('negatip');
-  // } else {
-  //   print('nol');
-  // }
+  switch(number){
+    case 0:
+      print("nol");
+      break;
+    case 1:
+      print("satu");
+      print(number + 10); break;
+    case 2:
+      print("dua"); break;
+    default:
+      print("bilangan lain");
 
-  //contoh lain if
-  // String output;
-  // if (number > 0){
-  //   output = 'positip';
-  // } else {
-  //   output = 'negatip atau nol';
-  // }
-  // print(output);
-
-  //contoh lain percabangan sederhana
-  String out;
-  out = (number > 0) ? "positif" : "negatif atau nol";
-      print(out);
+  }
 }
