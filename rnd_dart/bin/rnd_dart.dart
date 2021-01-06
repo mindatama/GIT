3e5da3085daed4d352b5a9a94c17bd978e6c7055
mindatama <mindatama@gmail.com>
2021-01-05 23:56:05 -0200

    folder bin. rnd_dart tambah contoh fungsi list

--- rnd_dart/bin/rnd_dart.dart
+++ rnd_dart/bin/rnd_dart.dart
@@ -1,9 +1,16 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
+  List<int> myliss = [];
   List<int> liss = [1, 2, 3];
   int nol = liss [0];
   print("${nol} \n");
+
+  myliss.add(10);
+  myliss.addAll(liss);
+  myliss.insert(0, 11);
+  myliss.insertAll(5, [90,80,70]);
+  myliss.clear();
 
   for (int idx = 0; idx < liss.length; idx += 1) {
     print(liss[idx]);
@@ -12,4 +19,9 @@ void main(List<String> arguments) {
   for(int bil in liss){
     print(bil);
   }
+  print("\n");
+
+  myliss.forEach((bil) {
+    print(bil);
+  });
 }
