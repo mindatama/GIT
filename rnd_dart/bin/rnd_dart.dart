cb1f71995eb93710d410546313156c8b6faba59c
mindatama <mindatama@gmail.com>
2021-01-05 23:05:14 -0200

    folder bin. rnd_dart mengubah contoh lambda expression atau simbol => (isinya harus nilai)

--- rnd_dart/bin/rnd_dart.dart
+++ rnd_dart/bin/rnd_dart.dart
@@ -1,16 +1,7 @@
 import 'dart:io';
 
-String saynamedparameter(String from, String message, {String to, String app}){
-return from + " say " + message + ((to != null ) ? " to " + to : "") + ((app != null ) ? " via app " + app : "");
-}
-String saypositionalparameter(String from, String message, [String to, String app]){
-  return from + " say " + message + ((to != null ) ? " to " + to : "") + ((app != null ) ? " via app " + app : "");
-}
-
-//named masukkan bisa tidak urut tapi harus kasih namenya, spt: app
-//positional masukkan harus urut tapi langsung bisa isinya, "nadia"
+int luas(int p, int l) => p * l;
 
 void main(List<String> arguments) {
-print(saynamedparameter("wijat", "hay", app: "telegram"));
-print(saypositionalparameter("koko", "pagi", "nadia"));
+print(luas(2,3));
 }
