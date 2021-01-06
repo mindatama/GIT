e85c9fd9813aa6deb5431f01121c40c4793c0bde
mindatama <mindatama@gmail.com>
2021-01-06 11:54:58 +0800

    folder bin. rnd_dart tambah contoh list mapping

--- rnd_dart/bin/rnd_dart.dart
+++ rnd_dart/bin/rnd_dart.dart
@@ -2,34 +2,38 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   List<int> myliss = [];
-  List<int> liss = [4, 5, 6, 1, 2, 3];
+  List<int> liss = [5, 5, 5, 1, 2, 3];
   // int nol = liss [0];
   // print("${nol} \n");
-
-  myliss.add(10);
-  myliss.addAll(liss);
-  myliss.insert(0, 11);
-  myliss.insertAll(5, [90,80,70]);
-  myliss.clear();
-  // liss.clear();
-   liss.sort();
-   liss.removeWhere((x) => x % 2 == 0);
-   if (liss.every((num) => num % 2 != 0)){
-  print("ganjil");
-  } else {
-    print("tidak semua ganjil");
-  }
+Set<int> bil;
+bil = liss.toSet();
+// if (liss.isNotEmpty) {
+//   print("tidak kosong");
+// }
+  // myliss.add(10);
+  // myliss.addAll(liss);
+  // myliss.insert(0, 11);
+  // myliss.insertAll(5, [90,80,70]);
+  // myliss.clear();
+  // // liss.clear();
+  //  liss.sort();
+  //  liss.removeWhere((x) => x % 2 == 0);
+  //  if (liss.every((num) => num % 2 != 0)){
+  // print("ganjil");
+  // } else {
+  //   print("tidak semua ganjil");
+  // }
 
   // for (int idx = 0; idx < liss.length; idx += 1) {
   //   print(liss[idx]);
   // }
   // print("\n");
-  for(int bil in liss){
-    print(bil);
-  }
+  // for(int bil in liss){
+  //   print(bil);
+  // }
   // print("\n");
   //
-  // myliss.forEach((bil) {
-  //   print(bil);
-  // });
+  bil.forEach((bil) {
+    print(bil);
+  });
 }
