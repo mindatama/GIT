d88d7cdb71151544bd02b9ce12c9b18010910d6a
mindatama <mindatama@gmail.com>
2021-01-06 21:32:57 +0800

    folder bin. rnd_dart ganti contoh implementasi class dan objek

--- rnd_dart/bin/rnd_dart.dart
+++ rnd_dart/bin/rnd_dart.dart
@@ -1,39 +1,26 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  List<int> myliss = [];
-  List<int> liss = [5, 5, 5, 1, 2, 3];
-  // int nol = liss [0];
-  // print("${nol} \n");
-Set<int> bil;
-bil = liss.toSet();
-// if (liss.isNotEmpty) {
-//   print("tidak kosong");
-// }
-  // myliss.add(10);
-  // myliss.addAll(liss);
-  // myliss.insert(0, 11);
-  // myliss.insertAll(5, [90,80,70]);
-  // myliss.clear();
-  // // liss.clear();
-  //  liss.sort();
-  //  liss.removeWhere((x) => x % 2 == 0);
-  //  if (liss.every((num) => num % 2 != 0)){
-  // print("ganjil");
-  // } else {
-  //   print("tidak semua ganjil");
-  // }
+persegi kotak1, kotak2;
+int luaskotak;
 
-  // for (int idx = 0; idx < liss.length; idx += 1) {
-  //   print(liss[idx]);
-  // }
-  // print("\n");
-  // for(int bil in liss){
-  //   print(bil);
-  // }
-  // print("\n");
-  //
-  bil.forEach((bil) {
-    print(bil);
-  });
+kotak1 = persegi();
+kotak1.panjang = 3;
+kotak1.lebar = 5;
+luaskotak = kotak1.luas();
+
+kotak2 = persegi();
+kotak2.panjang = int.tryParse(stdin.readLineSync());
+kotak2.lebar = int.tryParse(stdin.readLineSync());
+
+print(luaskotak +kotak2.luas());
+}
+
+class persegi {
+  int panjang;
+  int lebar;
+
+  int luas(){
+    return this.panjang * this.lebar;
+  }
 }
