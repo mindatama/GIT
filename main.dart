14c18c47c9a78814d423c56ab486840e6c83a237
wijatmoko <wijatmoko.m@bukitmakmur.com>
2021-01-12 18:54:54 +0800

    main.dart tambah print

--- main.dart
+++ main.dart
@@ -3,6 +3,5 @@ main() {
   final jargon = "bisa, harus bisa, pasti bisa";
   var hehe = "semangat pagi";
   var haha = 'hallo dunia,\' saya wijatmoko. $hehe';
-  print(haha);
-  print(jargon);
+  print(haha + jargon);
 }
