4a91c7cd55baf358136f319821bce83d3fde875e
mindatama <mindatama@gmail.com>
2021-01-12 13:20:46 +0800

    folder construct_demo. tambah folder construct_demo

---
+++ construct_demo/bin/construct_demo.dart
@@ -0,0 +1,5 @@
+import 'package:construct_demo/construct_demo.dart' as construct_demo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${construct_demo.calculate()}!');
+}
---
+++ construct_demo/test/construct_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:construct_demo/construct_demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
