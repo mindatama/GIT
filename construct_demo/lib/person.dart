901ac60aca21ede26072ee62978944a03a78c622
mindatama <mindatama@gmail.com>
2021-01-19 19:46:48 +0800

    tambah contoh constructor

--- construct_demo/lib/person.dart
+++ construct_demo/lib/person.dart
@@ -1,12 +1,10 @@
 class person {
   String name;
-<<<<<<< HEAD
-
   person({String name = 'no name'}) {
     this.name = name;
-=======
-  person() {
-    print('NIK person adalah: 10023543');
->>>>>>> bb7ad502f96069031221f81f1cde57b5a75090e9
+
+    person() {
+      print('NIK person adalah: 10023543');
+    }
   }
 }
