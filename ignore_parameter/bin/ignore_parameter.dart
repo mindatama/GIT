b9ce72f773b5504abaf532f6eb9584c94d7bac53
mindatama <mindatama@gmail.com>
2021-01-22 21:24:55 +0800

    tambah folder ignore_parameter

---
+++ ignore_parameter/bin/ignore_parameter.dart
@@ -0,0 +1,5 @@
+import 'package:ignore_parameter/ignore_parameter.dart' as ignore_parameter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ignore_parameter.calculate()}!');
+}
---
+++ ignore_parameter/test/ignore_parameter_test.dart
@@ -0,0 +1,8 @@
+import 'package:ignore_parameter/ignore_parameter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
