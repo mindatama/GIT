d71d45d377c32ba1d4ff578e5ae98f1a63c92567
mindatama <mindatama@gmail.com>
2021-01-31 12:44:24 +0800

    ignore contoh enum & cascade notation

--- ignore_parameter/bin/ignore_parameter.dart
+++ ignore_parameter/bin/ignore_parameter.dart
@@ -1,23 +1,35 @@
-const pi = 3.14;
-
 void main(List<String> arguments) {
-  var a = const cocl(number: 5);
-  var b = const cocl(number: 5);
-  print(identical(a, b));
-  print(pi);
+  ucoa m = ucoa(status: ucoastatus.poisoned);
+  print('hehe');
+
+  m
+    ..move()
+    ..eat();
 }
 
-class cocl {
-  final int number;
-  const cocl({this.number});
-}
+enum ucoastatus { normal, poisoned, confused }
 
-class rgcl {
-  final int number;
-  static const myc = 14;
+class ucoa {
+  final ucoastatus status;
 
-  rgcl({this.number}) {
-    const anco = 15;
-    print(anco);
+  ucoa({this.status = ucoastatus.normal});
+
+  void move() {
+    switch (status) {
+      case ucoastatus.normal:
+        print('moving');
+        break;
+      case ucoastatus.poisoned:
+        print('poisoning');
+        break;
+      case ucoastatus.confused:
+        print('spinning');
+        break;
+      default:
+    }
+  }
+
+  void eat() {
+    print('makan indomie');
   }
 }
