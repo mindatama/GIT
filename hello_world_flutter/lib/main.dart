f1df83e11c326102e643665ad047f4522e834912
mindatama <mindatama@gmail.com>
2021-02-18 07:51:02 +0800

    hello_world_flutter ganti contoh row& coloumn

--- hello_world_flutter/lib/main.dart
+++ hello_world_flutter/lib/main.dart
@@ -10,21 +10,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Aplikasi Melatih kemampuan Flutter'),
+          title: Text('Row dan coloumn'),
         ),
-        body: Center(
-            child: Container(
-                color: Colors.lightBlue,
-                width: 200,
-                height: 75,
-                child: Text(
-                  "Saya sedang melatih kemampuan flutter saya",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w100),
-                  overflow: TextOverflow.clip,
-                ))),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("data 1"),
+            Text("data 2"),
+            Text("data 3"),
+            Row(
+              children: <Widget>[Text("data 04"), Text("data 05")],
+            )
+          ],
+        ),
       ),
     );
   }
