817a3cc89904bb57a3a9e7bda6936d759d90d244
mindatama <mindatama@gmail.com>
2021-02-28 22:13:31 +0800

    hello_world_flutter ganti contoh draggable, dragTarget, SizedBox, Material

--- hello_world_flutter/lib/main.dart
+++ hello_world_flutter/lib/main.dart
@@ -10,6 +10,11 @@ class Myapp extends StatefulWidget {
 }
 
 class _MyappState extends State<Myapp> {
+  Color c1 = Colors.amber;
+  Color c2 = Colors.red;
+  Color tc;
+  bool ia = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,17 +26,95 @@ class _MyappState extends State<Myapp> {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                SizedBox(
-                  width: 200,
-                  height: 100,
-                  child: Material(
-                    color: Colors.amber,
-                    shape: StadiumBorder(),
-                    elevation: 3,
+                Draggable<Color>(
+                  data: c1,
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: c1,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: Colors.black26,
+                      shape: StadiumBorder(),
+                    ),
+                  ),
+                  feedback: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: c1.withOpacity(0.5),
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                ),
+                Draggable<Color>(
+                  data: c2,
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: c2,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: Colors.black26,
+                      shape: StadiumBorder(),
+                    ),
+                  ),
+                  feedback: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: c2.withOpacity(0.5),
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
                   ),
                 )
               ],
+            ),
+            DragTarget<Color>(
+              onWillAccept: (value) => true,
+              onAccept: (value) {
+                ia = true;
+                tc = value;
+              },
+              builder: (context, candidates, rejected) {
+                return (ia)
+                    ? SizedBox(
+                        width: 150,
+                        height: 150,
+                        child: Material(
+                          color: tc,
+                          shape: StadiumBorder(),
+                          elevation: 3,
+                        ),
+                      )
+                    : SizedBox(
+                        width: 150,
+                        height: 150,
+                        child: Material(
+                          color: Colors.black26,
+                          shape: StadiumBorder(),
+                          elevation: 3,
+                        ),
+                      );
+              },
             ),
           ],
         ),
