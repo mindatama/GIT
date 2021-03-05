089141f534ac6653040298abe5f80f6233b0980a
mindatama <mindatama@gmail.com>
2021-03-05 23:37:11 +0800

    hello_world_flutter lib/fourth_page.dart tambah page baru

---
+++ hello_world_flutter/lib/fourth_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FourthPage extends StatefulWidget {
+  @override
+  _FourthPageState createState() => _FourthPageState();
+}
+
+class _FourthPageState extends State<FourthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
